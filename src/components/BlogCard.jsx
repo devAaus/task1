97b523d589e4fbf4a3f575571445117f28eb6181
aayush <aausgh@gmail.com>
2024-05-19 23:10:41 +0545

    updated blogcard

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,7 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React, { useMemo } from 'react'
 import { Link } from 'react-router-dom';
 
 const BlogCard = ({ blog }) => {
+
+    const authorId = blog?.author;
+    const {
+        isLoading: isLoadingAuthor,
+        error: errorAuthor,
+        data: author,
+    } = useQuery({
+        queryKey: ['author', authorId],
+        queryFn: () =>
+            axios
+                .get(`${import.meta.env.VITE_SERVER_URL}/author/${authorId}`)
+                .then((res) => res.data),
+    });
 
     const formattedDate = useMemo(() => {
         const date = new Date(blog.createdAt);
@@ -12,6 +27,14 @@ const BlogCard = ({ blog }) => {
         });
     }, [blog.createdAt]);
 
+    if (isLoadingAuthor) {
+        return <div>Loading...</div>;
+    }
+
+    if (errorAuthor) {
+        return <div>Error: {errorAuthor.message}</div>;
+    }
+
     return (
         <div className='py-4 hover:scale-[1.005] transition-all duration-300 ease-in-out flex flex-col items-start justify-center cursor-pointer'>
             <Link to={`/blog/${blog._id}`}>
@@ -20,9 +43,10 @@ const BlogCard = ({ blog }) => {
                         {blog.title}
                     </h2>
 
-                    <div className='flex gap-2 font-medium'>
-                        <p className='text-xs text-gray-300 ml-1'>
-                            {blog.author}
+                    <div className='flex items-center gap-2 font-medium'>
+                        <p className='text-sm text-gray-300 ml-1'>
+                            by
+                            <span className='ml-2 text-base text-gray-200'>{author.fullName}</span>
                         </p>
 
                         <p className='text-xs text-gray-300 ml-1'>
