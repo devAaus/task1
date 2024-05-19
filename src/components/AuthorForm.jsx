0cc314622d7ee3331ed333af50f8b702cb164151
aayush <aausgh@gmail.com>
2024-05-19 16:13:41 +0545

    added authorform

---
+++ src/components/AuthorForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AuthorForm = ({ handlSubmitAuthor }) => {
+    return (
+        <dialog id="modal_2" className="modal">
+            <div className="modal-box">
+                <h2 className='text-4xl text-title font-bold mb-6'>
+                    Add Author
+                </h2>
+
+                <button
+                    className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                    onClick={() => document.getElementById('modal_2').close()}
+                >
+                    ✕
+                </button>
+
+                <form className='flex flex-col gap-4' onSubmit={handlSubmitAuthor}>
+
+                    <input type="text" placeholder="FullName" className="input input-bordered w-full max-w-xs" />
+
+                    <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs" />
+
+                    <button type="submit" className="btn btn-primary">Submit</button>
+
+                </form>
+            </div>
+        </dialog>
+    )
+}
+
+export default AuthorForm
