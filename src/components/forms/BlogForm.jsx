0b284c1be919109081c5f61bf7e788139ff2b41f
aayush <aausgh@gmail.com>
2024-05-20 12:38:45 +0545

    added create blog function

--- src/components/forms/BlogForm.jsx
+++ src/components/forms/BlogForm.jsx
@@ -1,28 +1,52 @@
 import React from 'react'
 
-const BlogForm = ({ handlSubmitBlog }) => {
+const BlogForm = ({ handleSubmit, title, setTitle, content, setContent, authorId, setAuthorId }) => {
 
     return (
 
 
         <form
             className='flex flex-col gap-4'
-            onSubmit={handlSubmitBlog}
+            onSubmit={handleSubmit}
         >
 
-            <input type="text" placeholder="Title" className="input input-bordered w-full max-w-xs" />
+            <input
+                type="text"
+                placeholder="Title"
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+            />
 
-            <select className="select select-bordered w-full max-w-xs">
+            {/* <select className="select select-bordered w-full max-w-xs">
                 <option disabled selected>Select Author</option>
                 <option>Game of Thrones</option>
                 <option>Lost</option>
                 <option>Breaking Bad</option>
                 <option>Walking Dead</option>
-            </select>
+            </select> */}
 
-            <textarea className="textarea textarea-bordered" placeholder="Description"></textarea>
+            <input
+                type='text'
+                value={authorId}
+                placeholder='Author Id'
+                onChange={(e) => setAuthorId(e.target.value)}
+                className='input input-bordered w-full max-w-xs'
+            />
 
-            <button type="submit" className="btn btn-primary">
+            <textarea
+                className="textarea textarea-bordered"
+                placeholder="Description"
+                name="content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+
+            <button
+                type="submit"
+                className="btn btn-primary"
+            >
                 Submit
             </button>
 
--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BlogForm from '../components/forms/BlogForm'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 const AddBlog = () => {
 
-    const handlSubmitBlog = (e) => {
-        e.preventDefault()
-    }
+    const queryClient = useQueryClient();
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [authorId, setAuthorId] = useState('');
+
+    const addBlogmutation = useMutation({
+        mutationFn: (newAuthor) => axios.post(`${import.meta.env.VITE_SERVER_URL}/blog`, newAuthor),
+
+        onSuccess: () => {
+            queryClient.invalidateQueries(['blogs']);
+            setTitle('');
+            setContent('');
+            setAuthorId('');
+        },
+
+        onError: (error) => {
+            console.log(error);
+        }
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newBlog = { title, content, authorId };
+        toast.promise(
+            addBlogmutation.mutateAsync(newBlog),
+            {
+                loading: 'Adding Blog...',
+                success: 'Blog Added!',
+                error: 'Error While Adding.',
+            }
+        );
+    };
 
     return (
         <div>
             <h2 className='text-4xl text-title font-bold mb-6'>
                 Add Blog
             </h2>
-            <BlogForm handlSubmitBlog={handlSubmitBlog} />
+            <BlogForm handleSubmit={handleSubmit} title={title} setTitle={setTitle} content={content} setContent={setContent} authorId={authorId} setAuthorId={setAuthorId} />
         </div>
     )
 }
